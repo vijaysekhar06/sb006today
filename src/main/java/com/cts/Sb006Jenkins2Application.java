e0c50febb48f0424775efb42142640182aeb1f81
swamy <palaganiayyappa@gmail.com>
2021-06-04 09:41:40 +0530

    first commit

---
+++ src/main/java/com/cts/Sb006Jenkins2Application.java
@@ -0,0 +1,22 @@
+package com.cts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Sb006Jenkins2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sb006Jenkins2Application.class, args);
+	}
+	
+	@GetMapping
+	public String ss()
+	{
+		return "Hello springapp............";
+	}
+
+}
---
+++ src/test/java/com/cts/Sb006Jenkins2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sb006Jenkins2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
